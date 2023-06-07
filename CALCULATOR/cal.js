1f1f15f05b2c51ea0ad2da1d60772bebb2550c94
DilshanaRanawake <dilshanaranawake@gmail.com>
2023-06-07 21:15:29 +0530

    Moving Calculator files to parent folder

--- CALCULATOR/cal.js
+++
@@ -1,7 +0,0 @@
-function Add(){
-    // alert("Button Test");
-    let fn= parseFloat(document.getElementById("txtFirstNumber").value);
-    let sn= parseFloat(document.getElementById("txtSecondNumber").value); 
-
-    document.getElementById("txtResult").value= fn+sn;
-}
