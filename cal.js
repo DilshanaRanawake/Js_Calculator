e3cabbc8c3ad47321667e99ee57132860f04ca39
DilshanaRanawake <dilshanaranawake@gmail.com>
2023-06-09 20:04:09 +0530

    add,sub,mul,div functions

--- cal.js
+++ cal.js
@@ -5,3 +5,24 @@ function Add(){
 
     document.getElementById("txtResult").value= fn+sn;
 }
+
+function Sub(){
+    let fn= parseFloat(document.getElementById("txtFirstNumber").value);
+    let sn= parseFloat(document.getElementById("txtSecondNumber").value); 
+
+    document.getElementById("txtResult").value= fn-sn;
+}
+
+function Mul(){
+    let fn= parseFloat(document.getElementById("txtFirstNumber").value);
+    let sn= parseFloat(document.getElementById("txtSecondNumber").value); 
+
+    document.getElementById("txtResult").value= fn*sn;
+}
+
+function Div(){
+    let fn= parseFloat(document.getElementById("txtFirstNumber").value);
+    let sn= parseFloat(document.getElementById("txtSecondNumber").value); 
+
+    document.getElementById("txtResult").value= fn/sn;
+}
